44d02f853cca1d58de9a8dbb3a08e6c9c6a1a3f1
N-N-Kareev <89892916659k@gmail.com>
2026-02-13 00:39:46 +0300

    [ip] Added Prisma module and service.

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client/extension';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.onModuleInit();
+  }
+  async onModuleDestroy() {
+    await this.onModuleDestroy();
+  }
+}
